482e9e120f78eef3210cc316c3672a3db864b390
katrien1213 <katrien1213@gmail.com>
2025-10-03 18:11:36 +0800

    Last activity

--- code.js
+++ code.js
@@ -1,16 +1,31 @@
-    var title= document.querySelector("h1");
-    title.innerHTML="This is the title from code.js";
+var title = document.querySelector("h1");
+title.innerHTML = "This is the title from code.js";
 
-    //fetch the button from the DOM
-    var button= document.querySelector("button")
+// fetch the button from the DOM
+var button = document.querySelector("#CV");
 
-    //attch and event when user clicks it 
-    button.addEventListener("click", myfunction);
+// attach an event when user clicks it 
+button.addEventListener("click", myfunction);
 
-    //create the function that will be called when the button is pressed 
-    function myfunction()
-    {
-        //this shows a popup window
-        alert("let me tell you more about me");
-        
-    }
+ 
+function myfunction() {
+    
+    alert("let me tell you more about me");
+}
+
+
+var mynode = document.createElement("div");
+mynode.id = "work1_intro";
+mynode.innerHTML = "The work is an exhibition";
+mynode.style.color = "blue";
+
+mynode.addEventListener("click", welcomeToWork1);
+document.querySelector("#my_work1").appendChild(mynode);
+
+
+
+
+
+function welcomeToWork1() {
+   mynode.innerHTML="Thank you for your interest in my work!"
+}
